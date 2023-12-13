e226b76c7b6160e912c5fb049b04406512157768
Nico Richard <nicolas.richard@justeattakeaway.com>
2023-12-13 16:25:11 -0600

    practical observable object test

---
+++ Tests/AsyncSpyTests/Practical/ObservableObjectTests.swift
@@ -0,0 +1,28 @@
+import Foundation
+import XCTest
+@testable import AsyncSpy
+
+private class State: ObservableObject {
+    @Published var username: String = ""
+    
+    func login() {
+        Task {
+            try await Task.sleep(nanoseconds: 10)
+            username = "Nico"
+        }
+    }
+}
+
+class ObservableObjectTests: XCTestCase {
+    func testViewModelStyleLogin() async throws {
+        let state = State()
+        
+        let spy = AsyncSpy(subject: state.$username)
+        
+        await spy.expect("")
+        
+        state.login()
+        
+        await spy.expect("Nico")
+    }
+}
